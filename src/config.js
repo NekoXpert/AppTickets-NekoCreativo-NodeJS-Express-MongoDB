6ceb8db4bc32fc7f66ee554d7451dfe797b36b05
Felipe Reyes <90102225+Nekosor@users.noreply.github.com>
2022-12-02 05:40:16 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://nekocreativo:PCGAMER20**@cluster0.ldxp9.mongodb.net/?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://nekocreativo:PC*****20**@clusternekocrea.bqvbrhr.mongodb.net/?retryWrites=true&w=majority";
