dc181f42a4bd0eeb2d3df4d82cde1668909be9f6
Felipe Reyes <90102225+Nekosor@users.noreply.github.com>
2023-03-29 22:38:10 -0500

    Update config.js
    Actualizar db for testing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://nekocreativo:PC*****20**@clusternekocrea.bqvbrhr.mongodb.net/?retryWrites=true&w=majority";
+export const PORT = process.env.PORT || 27017;
+// export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://nekocreativo:PC*****20**@clusternekocrea.bqvbrhr.mongodb.net/?retryWrites=true&w=majority";
+
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongodb:$exterNO2024@mongodb-q06w:27017/DB_PASSWORD"
+
