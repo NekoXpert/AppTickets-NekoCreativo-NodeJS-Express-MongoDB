601dd04e108a060bce59e37e61656ed393b15b1d
Felipe Reyes <90102225+Nekosor@users.noreply.github.com>
2023-03-29 23:05:59 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ config();
 
 export const PORT = process.env.PORT || 27017;
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongodb:$exterNO2024@mongodb-q06w/DB_PASSWORD";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongodb-q06w:27017/?compressors=disabled&gssapiServiceName=mongodb";
 
